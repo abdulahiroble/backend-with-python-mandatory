725796a465de9cfbb18b2977468cb39abed7966e
abdulahi roble <abdulahi_2610@hotmail.com>
2023-03-24 11:34:19 +0100

    All required packages

--- bank_mandatory_app/admin.py
+++ bank_mandatory_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Account, Employee, Group, Loans
+from .models import Customer, Account, Employee, Group, Ledger
 
 admin.site.register(Customer)
 admin.site.register(Account)
 admin.site.register(Employee)
 admin.site.register(Group)
-admin.site.register(Loans)
+admin.site.register(Ledger)
 
